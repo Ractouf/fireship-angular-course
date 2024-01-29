aa13d95dcf17799c82712775df69df0bfbcd6959
francois.vandeputte <francois.vandeputte@student.vinci.be>
2024-01-29 21:01:00 +0100

    1-11 fireship angular course

---
+++ myapp/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routes';
+import { BoardComponent } from './board/board.component';
+import { SquareComponent } from './square/square.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SquareComponent,
+    BoardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
